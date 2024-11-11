70e15fb6e8f0f5b748d164d51c2e83067e81679c
Akrit <akritrihal57@gmail.com>
2024-11-11 12:29:04 +0530

    components

---
+++ app/components/PromptSuggestionsRow.tsx
@@ -0,0 +1,23 @@
+import PromptSuggestionButton from "./PromptSuggestionButton"
+
+const PromptSuggestionsRow = (onPromptClick)=>{
+
+    const prompts = [
+        "Who is head of racing for Aston Martin's Fl Academy team?",
+        "Who is the highest paid Fl driver?",
+        "Who will be the newest driver for Ferrari?",
+        "Who is the current Formula One World Driver's Champion?"
+    ]
+
+    return (
+        <div className="prompt-suggestion-row">
+            {prompts.map((prompt, index)=>
+                <PromptSuggestionButton key = {`suggestion-${index}`}
+                text = {prompt}
+                onClick = {() => onPromptClick(prompt)}
+            />)}
+        </div>
+    )
+}
+
+export default PromptSuggestionsRow
---
+++ app/page.tsx
@@ -0,0 +1,55 @@
+
+"use client"
+import Image from "next/image";
+import F1Logo from "./assets/F1Lens2.png"
+import { useChat } from "ai/react"
+import { Message } from "ai"
+import Bubble from "./components/Bubble";
+import LoadingBubble from "./components/LoadingBubble";
+import PromptSuggestionsRow from "./components/PromptSuggestionsRow";
+import { Messages } from "openai/resources/beta/threads/messages.mjs";
+
+
+const Home = ()=>{
+
+    const { append, isLoading, messages, input, handleInputChange, handleSubmit } = useChat() 
+
+    const noMessages = !messages || messages.length === 0;
+
+    const handlePrompt = (promptText) =>{
+        const msg: Message = {
+            id: crypto.randomUUID(),
+            content: promptText  ,
+            role: 'user'
+        }
+        append(msg)
+    }
+
+    return(
+        <main>
+            <Image src = {F1Logo} width = "250" alt = "F1GPT Logo"></Image>
+            <section className={noMessages? "": "populated"}>
+                {noMessages ? (
+                    <>
+                        <p className = "starter-text">
+                            F1GPT: Your Ultimate F1 Game Companion! Get fast, reliable answers to all your questions about the F1 games. From game strategies and car setups to driver insights and race simulations, F1GPT is here to elevate your gaming experience and help you stay ahead of the competition.
+                        </p>
+                        <br/>
+                        <PromptSuggestionsRow onPromptClick={handlePrompt}/>
+                    </>
+                ):(
+                    <>
+                        {messages.map((message, index)=> <Bubble key = {`message-${index}`}message={message}/>)}
+                        {isLoading && <LoadingBubble/>}
+                    </>
+                )}
+            </section>
+
+            <form onSubmit={handleSubmit}>
+                <input className="question-box" onChange = {handleInputChange} value = {input} placeholder= "Ask me something !"/>
+            </form>
+
+        </main>
+    )
+}
+export default Home;
